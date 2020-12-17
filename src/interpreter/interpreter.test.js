cf4124841ad5afc7079be69f396ac6473fc39fd8
connorwalsh <c@polygon.pizza>
2020-12-17 16:51:04 -0500

    introduces test stubs for features to test

--- src/interpreter/interpreter.test.js
+++ src/interpreter/interpreter.test.js
@@ -55,12 +55,12 @@ describe('the interpreter', () => {
   describe('interpreting single statements', () => {
     describe(' using lexer->parser1->parser2', () => {
       
-      describe('import statements', () => {
+      describe('on import statements', () => {
         it.todo(`imports stuff`)
       })
 
-      describe('sequence statements', () => {
-        it(`interprets 'apple orange pear'`, () => {
+      describe('on sequence statements', () => {
+        it(`can intepret: apple orange pear`, () => {
           const txt = `apple orange pear`
           const interpret = newTestInterpreter()
 
@@ -75,6 +75,54 @@ describe('the interpreter', () => {
 
           expect(interpret(txt)).toEqual(expectations)
         })
+
+        it.todo(`can interpret: (A B C)`)
+        it.todo(`can interpret: [A B C]`)
+        it.todo(`can interpret: (A [B C])`)
+        it.todo(`can interpret: [A (B C)]`)
+        it.todo(`can interpret: [A (B C)]`)
+        it.todo(`can interpret: [A (B C)]`)
+        it.todo(`can interpret: "A B" C`)
+        it.todo(`can interpret: 'A B' C`)
+        it.todo(`can interpret: '"A B"' C`)
+        it.todo(`can interpret: A * 3`)
+        it.todo(`can interpret: 3 * A`)
+        it.todo(`can interpret: (A B C) * 3`)
+        it.todo(`can interpret: 3 * (A B C)`)
+        it.todo(`can interpret: 3 * [A B C]`)
+        it.todo(`can interpret: [A B C] * 3`)
+        it.todo(`can interpret: [A B C] * 3`)
+        it.todo(`can interpret: [A B * 3 C]`)
+        it.todo(`can interpret: A.reverb`)
+        it.todo(`can interpret: A.reverb.volume`)
+        it.todo(`can interpret: A.reverb(time: 3.3)`)
+        it.todo(`can interpret: A.reverb(3.3)`)
+        it.todo(`can interpret: (A B C).reverb.volume`)
+        it.todo(`can interpret: [A B C].reverb.volume`)
+        it.todo(`can interpret: A.reverb().volume`)
+        it.todo(`can interpret: A.reverb(3.3, decay: 2).volume`)
+        it.todo(`can interpret: A(440Hz)`)
+        it.todo(`can interpret: A(note:440Hz)`)
+        it.todo(`can interpret: A(note:440Hz, isolated, loop)`)
+        it.todo(`can interpret: A(note:440Hz, isolated, loop).reverb`)
+        it.todo(`can interpret: A(note:440Hz, isolated, loop).reverb`)
+        it.todo(`can interpret: ? * A`) // ? is re-evaluated every time its called
+        it.todo(`can interpret: ?[0-4] * A`) // random within range
+        it.todo(`can interpret: ?[4, 6, 8, 3] * A`) // random ints
+        it.todo(`can interpret: ?[0-4, 100-90, 20.1-2.2, 3] * A`) // random list of ranges and ints
+        it.todo(`can interpret: 3 * 3 * A`)
+        it.todo(`can interpret: ? * 3 * A`)
+        it.todo(`can interpret: A.reverb(?)`) // will only evaluate once at function initialization
+        it.todo(`can interpret: A.reverb(sine(1hz))`) // oscillate the default parameter at 1hz (between 0 - 1)
+        it.todo(`can interpret: A.reverb(3 * sine(1hz))`) // oscillate the default parameter at 1hz (between 0 - 3)
+        it.todo(`can interpret: A.reverb(3 * sine(1hz) + 2)`) // oscillate the default parameter at 1hz (between 2 - 5)
+        it.todo(`can interpret: A.reverb(3 * sine(1hz) + sine(2hz) + 2)`) // oscillate the default parameter with sum of sinusoids
+        it.todo(`can interpret: A.reverb(3 * triangle(1hz))`) // oscillate the default parameter at 1hz (between 0 - 3)
+        it.todo(`can interpret: A.reverb(3 * square(1hz))`) // oscillate the default parameter at 1hz (between 0 - 3)
+        it.todo(`can interpret: A.reverb(3 * triangle(1hz) + sine(2hz) + 2)`) // oscillate the default parameter with sum of sinusoids (additive....you can just connect multiple lfo oscillators to parameters...)
+        it.todo(`can interpret: A.reverb(sine(4 * sine(2Hz)))`) // fm oscillation! see https://medium.com/@danielmckemie/tips-and-techniques-for-using-the-web-audio-api-89b8beda6cf2
+        it.todo(`can interpret: sine(440Hz)`) // an oscillator sound! in addition to sound samples....
+        it.todo(`can interpret: sine(?(440,500)hz) * A`) // hmm?
       })
       
     })
